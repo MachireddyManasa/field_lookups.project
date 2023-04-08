7d5c84efa32ecc5fef921c9b606978a38e5f9bf7
MachireddyManasa <machireddymanasa03@gmail.com>
2023-04-08 18:37:44 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from app.models import *
 # Create your views here.
 from django.db.models.functions import Length
+from django.db.models import Q
 
 
 def display_topics(request):
@@ -18,6 +19,18 @@ def display_webpages(request):
     LOW=Webpage.objects.all().order_by('-name')
     LOW=Webpage.objects.all().order_by(Length('name'))
     LOW=Webpage.objects.all().order_by(Length('name').desc())
+    LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(name__startswith='d')
+    LOW=Webpage.objects.filter(email__endswith='.com')
+    LOW=Webpage.objects.filter(name__contains='S')
+    LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(name__in=('manasa','pavan'))
+    LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(Q(topic_name='cricket') & Q(name='dhoni'))
+    LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(Q(topic_name='cricket'))
+    
+
     d={'webpages':LOW}
     return render(request,'display_webpages.html',d)
 def display_access(request):
@@ -26,5 +39,9 @@ def display_access(request):
     LOA=AccessRecord.objects.filter(date__gte='2023-06-10')
     LOA=AccessRecord.objects.filter(date__lt='2023-06-10')
     LOA=AccessRecord.objects.filter(date__lte='2023-06-10')
+    LOA=AccessRecord.objects.filter(date__year='2023')
+    LOA=AccessRecord.objects.filter(date__month='6')
+    LOA=AccessRecord.objects.filter(date__day='10')
+    LOA=AccessRecord.objects.filter(date__year__gt='2022')
     d={'access':LOA}
     return render(request,'display_access.html',d)
